210f563f647c34f6f5c524dbfd56a2915936cc9a
Diana <dianaprg27@gmail.com>
2021-09-22 21:13:50 -0500

    Inicio proyecto

---
+++ TiendaVirtualG7/src/main/java/com/mintic/TiendaVirtualG7/TiendaVirtualG7Application.java
@@ -0,0 +1,13 @@
+package com.mintic.TiendaVirtualG7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaVirtualG7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaVirtualG7Application.class, args);
+	}
+
+}
---
+++ TiendaVirtualG7/src/test/java/com/mintic/TiendaVirtualG7/TiendaVirtualG7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mintic.TiendaVirtualG7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVirtualG7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
