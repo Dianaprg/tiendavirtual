8210849ab58fc497282c9d6359e3742b74fbb2e7
Diana <dianaprg27@gmail.com>
2021-09-22 21:22:21 -0500

    Inicio proyecto

---
+++ TiendaVirtualG7/src/main/java/com/mintic/TiendaVirtualG7/Dao/UsuariosDAO.java
@@ -0,0 +1,9 @@
+package com.mintic.TiendaVirtualG7.Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mintic.TiendaVirtualG7.modelo.Usuarios;
+
+public interface UsuariosDAO extends JpaRepository<Usuarios, Integer>{
+
+}
---
+++ TiendaVirtualG7/src/main/java/com/mintic/TiendaVirtualG7/api/UsuariosAPI.java
@@ -0,0 +1,45 @@
+package com.mintic.TiendaVirtualG7.api;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mintic.TiendaVirtualG7.Dao.UsuariosDAO;
+import com.mintic.TiendaVirtualG7.modelo.Usuarios;
+
+@RestController
+@RequestMapping("usuarios")
+public class UsuariosAPI {
+	
+	@Autowired
+	private UsuariosDAO usuariosDAO;
+	
+	@PostMapping("/guardar")
+	
+	public void guardar(@RequestBody Usuarios usuarios) {
+		usuariosDAO.save(usuarios);
+	}
+	@GetMapping("/listar")
+	public List<Usuarios> listar(){
+		return usuariosDAO.findAll();
+		
+	}
+	@DeleteMapping("/eliminar/{id}")
+	
+	public void eliminar (@PathVariable("id")Integer id) {
+		usuariosDAO.deleteById(id);
+	}
+	@PutMapping("/actualizar")
+	public void actualizar(@RequestBody Usuarios usuarios) {
+		usuariosDAO.save(usuarios);
+	}
+
+}
+
